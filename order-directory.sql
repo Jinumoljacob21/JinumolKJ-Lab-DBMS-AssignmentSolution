8b24c7fe7dbb8f23e6ba110cf0e98c1cb29ba36d
Jinumol Jacob <93193544+Jinumoljacob21@users.noreply.github.com>
2022-02-14 21:34:31 +0530

    Add files via upload

---
+++ order-directory.sql
@@ -0,0 +1,177 @@
+Create Database if not exists `order-directory` ;
+use `order-directory`;
+
+create table if not exists `supplier`(
+`SUPP_ID` int primary key,
+`SUPP_NAME` varchar(50) ,
+`SUPP_CITY` varchar(50),
+`SUPP_PHONE` varchar(10)
+);
+
+CREATE TABLE IF NOT EXISTS `customer` (
+  `CUS_ID` INT NOT NULL,
+  `CUS_NAME` VARCHAR(20) NULL DEFAULT NULL,
+  `CUS_PHONE` VARCHAR(10),
+  `CUS_CITY` varchar(30) ,
+  `CUS_GENDER` CHAR,
+  PRIMARY KEY (`CUS_ID`));
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `CAT_ID` INT NOT NULL,
+  `CAT_NAME` VARCHAR(20) NULL DEFAULT NULL,
+ 
+  PRIMARY KEY (`CAT_ID`)
+  );
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `PRO_ID` INT NOT NULL,
+  `PRO_NAME` VARCHAR(20) NULL DEFAULT NULL,
+  `PRO_DESC` VARCHAR(60) NULL DEFAULT NULL,
+  `CAT_ID` INT NOT NULL,
+  PRIMARY KEY (`PRO_ID`),
+  FOREIGN KEY (`CAT_ID`) REFERENCES CATEGORY (`CAT_ID`)
+  
+  );
+
+CREATE TABLE IF NOT EXISTS `product_details` (
+  `PROD_ID` INT NOT NULL,
+  `PRO_ID` INT NOT NULL,
+  `SUPP_ID` INT NOT NULL,
+  `PROD_PRICE` INT NOT NULL,
+  PRIMARY KEY (`PROD_ID`),
+  FOREIGN KEY (`PRO_ID`) REFERENCES PRODUCT (`PRO_ID`),
+  FOREIGN KEY (`SUPP_ID`) REFERENCES SUPPLIER(`SUPP_ID`)
+  
+  );
+
+  
+  
+  CREATE TABLE IF NOT EXISTS `orders` (
+  `ORD_ID` INT NOT NULL,
+  `ORD_AMOUNT` INT NOT NULL,
+  `ORD_DATE` DATE,
+  `CUS_ID` INT NOT NULL,
+  `PROD_ID` INT NOT NULL,
+  PRIMARY KEY (`ORD_ID`),
+  FOREIGN KEY (`CUS_ID`) REFERENCES CUSTOMER(`CUS_ID`),
+  FOREIGN KEY (`PROD_ID`) REFERENCES PRODUCT_DETAILS(`PROD_ID`)
+  );
+
+CREATE TABLE IF NOT EXISTS `rating` (
+  `RAT_ID` INT NOT NULL,
+  `CUS_ID` INT NOT NULL,
+  `SUPP_ID` INT NOT NULL,
+  `RAT_RATSTARS` INT NOT NULL,
+  PRIMARY KEY (`RAT_ID`),
+  FOREIGN KEY (`SUPP_ID`) REFERENCES SUPPLIER (`SUPP_ID`),
+  FOREIGN KEY (`CUS_ID`) REFERENCES CUSTOMER(`CUS_ID`)
+  );
+  
+  
+insert into `supplier` values(1,"Rajesh Retails","Delhi",'1234567890');
+insert into `supplier` values(2,"Appario Ltd.","Mumbai",'2589631470');
+insert into `supplier` values(3,"Knome products","Banglore",'9785462315');
+insert into `supplier` values(4,"Bansal Retails","Kochi",'8975463285');
+insert into `supplier` values(5,"Mittal Ltd.","Lucknow",'7898456532');
+
+INSERT INTO `CUSTOMER` VALUES(1,"AAKASH",'9999999999',"DELHI",'M');
+INSERT INTO `CUSTOMER` VALUES(2,"AMAN",'9785463215',"NOIDA",'M');
+INSERT INTO `CUSTOMER` VALUES(3,"NEHA",'9999999999',"MUMBAI",'F');
+INSERT INTO `CUSTOMER` VALUES(4,"MEGHA",'9994562399',"KOLKATA",'F');
+INSERT INTO `CUSTOMER` VALUES(5,"PULKIT",'7895999999',"LUCKNOW",'M');
+
+INSERT INTO `CATEGORY` VALUES( 1,"BOOKS");
+INSERT INTO `CATEGORY` VALUES(2,"GAMES");
+INSERT INTO `CATEGORY` VALUES(3,"GROCERIES");
+INSERT INTO `CATEGORY` VALUES (4,"ELECTRONICS");
+INSERT INTO `CATEGORY` VALUES(5,"CLOTHES");
+
+INSERT INTO `PRODUCT` VALUES(1,"GTA V","DFJDJFDJFDJFDJFJF",2);
+INSERT INTO `PRODUCT` VALUES(2,"TSHIRT","DFDFJDFJDKFD",5);
+INSERT INTO `PRODUCT` VALUES(3,"ROG LAPTOP","DFNTTNTNTERND",4);
+INSERT INTO `PRODUCT` VALUES(4,"OATS","REURENTBTOTH",3);
+INSERT INTO `PRODUCT` VALUES(5,"HARRY POTTER","NBEMCTHTJTH",1);
+
+INSERT INTO PRODUCT_DETAILS VALUES(1,1,2,1500);
+INSERT INTO PRODUCT_DETAILS VALUES(2,3,5,30000);
+INSERT INTO PRODUCT_DETAILS VALUES(3,5,1,3000);
+INSERT INTO PRODUCT_DETAILS VALUES(4,2,3,2500);
+INSERT INTO PRODUCT_DETAILS VALUES(5,4,1,1000);
+
+
+INSERT INTO `ORDERS` VALUES (50,2000,"2021-10-06",2,1);
+INSERT INTO `ORDERS` VALUES(20,1500,"2021-10-12",3,5);
+INSERT INTO `ORDERS` VALUES(25,30500,"2021-09-16",5,2);
+INSERT INTO `ORDERS` VALUES(26,2000,"2021-10-05",1,1);
+INSERT INTO `ORDERS` VALUES(30,3500,"2021-08-16",4,3);
+
+INSERT INTO `RATING` VALUES(1,2,2,4);
+INSERT INTO `RATING` VALUES(2,3,4,3);
+INSERT INTO `RATING` VALUES(3,5,1,5);
+INSERT INTO `RATING` VALUES(4,1,3,2);
+INSERT INTO `RATING` VALUES(5,4,5,4);
+
+
+-- 3)	Display the number of the customer group by their genders who have placed any order of amount greater than or equal to Rs.3000.
+
+SELECT C.CUS_GENDER, count(C.CUS_GENDER)as COUNT from Customer as C 
+INNER JOIN  orders as O on  C.CUS_ID=O.CUS_ID where O.ORD_AMOUNT>=3000 
+group by C.CUS_GENDER;
+
+-- 4)	Display all the orders along with the product name ordered by a customer having Customer_Id=2.
+
+SELECT  O.ORD_ID,O.ORD_AMOUNT ,O.ORD_DATE,O.CUS_ID,O.PROD_ID,P.PRO_NAME 
+FROM orders AS O, PRODUCT_DETAILS AS PD,PRODUCT  AS P 
+WHERE O.PROD_ID = PD.PROD_ID AND PD.PRO_ID = P.PRO_ID AND O.CUS_ID = 2;
+
+-- 5)	Display the Supplier details who can supply more than one product
+
+select s.SUPP_ID,s.SUPP_NAME,s.SUPP_CITY,s.SUPP_PHONE 
+from supplier as s ,product_details as pd 
+where s.SUPP_ID=pd.SUPP_ID having count(pd.SUPP_ID)>1;
+
+
+-- 6)	Find the category of the product whose order amount is minimum
+
+SELECT C.CAT_ID,C.CAT_NAME FROM orders AS O 
+ INNER JOIN product_details AS PD ON O.PROD_ID=PD.PROD_ID 
+ INNER JOIN product AS P ON PD.PRO_ID=P.PRO_ID 
+ INNER JOIN category AS C ON P.CAT_ID =C.CAT_ID having  MIN(O.ORD_AMOUNT);
+
+-- 7)	Display the Id and Name of the Product ordered after “2021-10-05”.
+
+select product. PRO_ID,product. PRO_NAME FROM product,orders,product_details 
+where product.PRO_ID=product_details.PRO_ID and product_details.PROD_ID=orders.PROD_ID 
+AND orders. ORD_DATE>"2021-10-05";
+
+
+-- 8)	Display customer name and gender whose names start or end with character 'A'.
+
+select CUS_NAME,CUS_GENDER from customer where CUS_NAME like 'A%' or CUS_NAME LIKE '%A';
+
+
+-- 9)	Create a stored procedure to display the Rating for a Supplier if any along with the Verdict on that rating if any like if rating >4 then “Genuine Supplier” if rating >2 “Average Supplier” else “Supplier should not be considered”.
+SELECT S.SUPP_ID,S.SUPP_NAME,R.RAT_RATSTARS,
+
+case
+WHEN R.RAT_RATSTARS>4 THEN "Genuine Supplier"
+WHEN R.RAT_RATSTARS>2 THEN "Average Supplier"
+ELSE "Supplier should not be considered"
+END AS Verdict FROM rating as R inner join supplier as S ON S.SUPP_ID=R.SUPP_ID;
+
+call proc1;
+
+/* Answer is 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `proc1`()
+BEGIN
+SELECT S.SUPP_ID,S.SUPP_NAME,R.RAT_RATSTARS,
+case
+WHEN R.RAT_RATSTARS>4 THEN "Genuine Supplier"
+WHEN R.RAT_RATSTARS>2 THEN "Average Supplier"
+ELSE "Supplier should not be considered"
+END AS Verdict FROM rating as R inner join supplier as S ON S.SUPP_ID=R.SUPP_ID;
+
+END
+
+call proc1;
+*/
